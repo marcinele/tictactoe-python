f0db5c364cfbdbaa5fac2791738e727d195d595e
marcin <marcin>
2020-03-24 19:01:21 +0100

    added moves, need to fix check

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 import tictactoe
-
-
+import sys
+import os
 
 if __name__ == "__main__":
-    board = tictactoe.Board(4)
+    if sys.platform == "win32":
+        def win_clear():
+            os.system("cls")
+    elif sys.platform == "linux":
+        def win_clear():
+            os.system("clear")
+    win_clear()
+    board = tictactoe.Board(3)
     board.print_board()
-    player1 = tictactoe.Player("X")
-    player1.moveX = 4
-    player1.moveY = 2
-    board.get_move(player1)
-    board.print_board()
+    num = 1
+    sign = input("Set player 1 sign: ")
+    player1 = tictactoe.Player(sign, num)
+    num += 2
+    sign = input("Set player 2 sign: ")
+    player2 = tictactoe.Player(sign, num)
+    i = 1
+    while board.finished != True:
+        if i%2 == 1:
+            print("Player 1 move: ")
+            player1.moveX = int(input("Pos X: "))
+            player1.moveY = int(input("Pos Y: "))
+            board.get_move(player1)
+            win_clear()
+            board.print_board()
+            board.check(player1, player2)
+        else:
+            print("Player 2 move: ")
+            player2.moveX = int(input("Pos X: "))
+            player2.moveY = int(input("Pos Y: "))
+            board.get_move(player2)
+            win_clear()
+            board.print_board()
+            board.check(player1, player2)
+        i += 1
--- tictactoe.py
+++ tictactoe.py
@@ -4,19 +4,20 @@ class Board:
     finished = True
     def __init__(self,_size):
         self.size = _size
-        self.board = [[" " for x in range(4*self.size+1)] for y in range(self.size+2)]
+        self.board = [[" " for x in range(4*self.size+1)] for y in range(2*self.size+1)]
         self.build_board()
         self.finished = False
 
     def build_board(self):
         x = 4*self.size+1
-        y = self.size+2
+        y = 2*self.size+1
         for i in range(0,x):
-            self.board[0][i] = "-"
-            self.board[y-1][i] = "-"
             if i%4 == 0  :
                 for j in range(1,y-1):
                     self.board[j][i] = "|"
+        for i in range(0,y,2):
+            for j in range(0,x):
+                self.board[i][j] = "-"
 
     def print_board(self):
         for x in self.board:
@@ -25,14 +26,51 @@ class Board:
             print("")
 
     def get_move(self, player):
-        self.board[player.moveY][4*player.moveX-2] = player.sign
+        self.board[2*player.moveY-1][4*player.moveX-2] = player.sign
+
+    def check(self, player1, player2): #only for 3x3 board
+        temp_list_x = []
+        temp_list_y = []
+        for i in range(2,4*self.size-1,4):
+            for j in range(1, 2*self.size+1, 2):
+                temp_list_y.append(self.board[j][i])
+            self.check_of_check(temp_list_y, player1, player2)
+            temp_list_y = []
+        for i in range(1, 2*self.size+1, 2):
+            for j in range(2,4*self.size-1,4):
+                temp_list_x.append(self.board[i][j])
+            self.check_of_check(temp_list_x, player1, player2)
+            temp_list_x = []
+        for i in range(2,4*self.size-1,4):
+            for j in range(1, 2*self.size+1, 2):
+                temp_list_y.append(self.board[j][i])
+            self.check_of_check(temp_list_y, player1, player2)
+            temp_list_y = []
+        temp_list_x.append(self.board[1][10])
+        temp_list_x.append(self.board[3][6])
+        temp_list_x.append(self.board[5][2])
+        self.check_of_check(temp_list_x, player1, player2)
+        temp_list_x = []
+
+    def check_of_check(self, temp_list, player1, player2):
+        if temp_list.count(player1.sign) == len(temp_list):
+            print("Game over. Player ", player1.number, " (", player1.sign, ") won.")
+            self.finished = True
+        elif temp_list.count(player2.sign) == len(temp_list):
+            print("Game over. Player ", player2.number, " (", player2.sign, ") won.")
+            self.finished = True
+        temp_list = []
+
+
 
 
 class Player:
+    number = 0
     sign = "X"
     moveX = 0
     moveY = 0
-    def __init__(self, _sign):
+    def __init__(self, _sign, _number):
         self.sign = _sign
         moveX = 999
         moveY = 999
+        number = _number
